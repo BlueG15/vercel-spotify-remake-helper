44df052bfa0c27d0ad0266584f076b6c334bf794
Blu <90506743+BlueG15@users.noreply.github.com>
2024-11-08 21:37:23 +0700

    Delete api directory

--- api/index.ts
+++
@@ -1,4 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).send("Hello, welcome to the api I made~");
-}
